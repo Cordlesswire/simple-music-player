d3ba4f50157a98d7988fe6f61d0d99eed5e6783e
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-05-07 22:12:52 +0700

    Add function start & pause

--- app/src/main/java/com/example/android/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/android/musicplayer/MainActivity.java
@@ -1,24 +1,28 @@
 package com.example.android.musicplayer;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mediaPlayer = MediaPlayer.create(this, R.raw.bensound_epic);
+
         Button playButton = (Button)findViewById(R.id.play_button);
 
         playButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Play", Toast.LENGTH_SHORT).show();
+                mediaPlayer.start();
             }
         });
 
@@ -27,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
         pauseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Pause", Toast.LENGTH_SHORT).show();
+                mediaPlayer.pause();
             }
         });
     }
